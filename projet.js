e40fe60cf123ed4a226f62889cd4330d8698064c
ssaeli <63305108+ssaeli@users.noreply.github.com>
2020-04-10 14:36:38 +0200

    Project changes
    changes

--- projet.js
+++ projet.js
@@ -1,48 +1,52 @@
-const width = 400;
-const height = 33;
+import {atan, exp, halfPi, log, pi, tan, tau} from "../math.js";
+import rotation from "../rotation.js";
+import projection from "./index.js";
 
-const svg = d3.select('body')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height)
-  .attr('viewBox', `0 -20 ${width} ${height}`);
-
-function update() {
-  const t = svg.transition()
-  .duration(750)
-
-  svg.selectAll('text')
-    .data(randomLetters(), d => d)
-    .join(
-      enter => enter.append('text')
-      .attr('fill', 'green')
-      .attr('x', (d, i) => i * 16)
-      .attr('y', -30)
-      .text(d => d)
-      .call(enter => enter.transition(t)
-            .attr('y', 0)),
-      update => update
-      .attr('fill', 'black')
-      .attr('y', 0)
-      .call(update => update.transition(t)
-            .attr('x', (d, i) => i * 16)),
-      exit => exit
-      .attr('fill', 'brown')
-      .call(exit => exit.transition(t)
-            .attr('y', 30)
-            .remove())
-  );
+export function mercatorRaw(lambda, phi) {
+  return [lambda, log(tan((halfPi + phi) / 2))];
 }
 
-setInterval(update, 2500);
+mercatorRaw.invert = function(x, y) {
+  return [x, 2 * atan(exp(y)) - halfPi];
+};
 
-
-// Utility functions
-function randomLetters() {
-  return d3.shuffle('abcdefghijklmnopqrstuvwxyz'.split(''))
-    .slice(0, Math.floor(6 + Math.random() * 20))
-    .sort();
+export default function() {
+  return mercatorProjection(mercatorRaw)
+      .scale(961 / tau);
 }
 
-d3.geoMercator()
-d3.geoMercatorRaw
+export function mercatorProjection(project) {
+  var m = projection(project),
+      center = m.center,
+      scale = m.scale,
+      translate = m.translate,
+      clipExtent = m.clipExtent,
+      x0 = null, y0, x1, y1; // clip extent
+
+  m.scale = function(_) {
+    return arguments.length ? (scale(_), reclip()) : scale();
+  };
+
+  m.translate = function(_) {
+    return arguments.length ? (translate(_), reclip()) : translate();
+  };
+
+  m.center = function(_) {
+    return arguments.length ? (center(_), reclip()) : center();
+  };
+
+  m.clipExtent = function(_) {
+    return arguments.length ? ((_ == null ? x0 = y0 = x1 = y1 = null : (x0 = +_[0][0], y0 = +_[0][1], x1 = +_[1][0], y1 = +_[1][1])), reclip()) : x0 == null ? null : [[x0, y0], [x1, y1]];
+  };
+
+  function reclip() {
+    var k = pi * scale(),
+        t = m(rotation(m.rotate()).invert([0, 0]));
+    return clipExtent(x0 == null
+        ? [[t[0] - k, t[1] - k], [t[0] + k, t[1] + k]] : project === mercatorRaw
+        ? [[Math.max(t[0] - k, x0), y0], [Math.min(t[0] + k, x1), y1]]
+        : [[x0, Math.max(t[1] - k, y0)], [x1, Math.min(t[1] + k, y1)]]);
+  }
+
+  return reclip();
+}
